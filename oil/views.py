29cfe671bef3e2ff6ea6ed4f4a97808c37aeeace
ma <ma_yamaki@yahoo.com>
2021-03-08 13:26:05 +0900

    do something

--- oil/views.py
+++ oil/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World. You are at the index of oil.")
+
+def detail(request, question_id):
+    return HttpResponse("You are at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are lokking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
