fb964fa8aeaa4d41efb3ed8f3f7ac029ff73b055
ma <ma_yamaki@yahoo.com>
2021-03-08 15:39:34 +0900

    dd

---
+++ jup/.virtual_documents/Untitled.ipynb.py
@@ -0,0 +1,13 @@
+import time
+
+
+from django.utils import timezone
+
+
+import datetime
+
+
+from oil.models import Choice, Question
+
+
+pub_date = 
--- oil/views.py
+++ oil/views.py
@@ -1,3 +1,5 @@
+#from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 #from django.template import loader
 from django.shortcuts import render
@@ -6,6 +8,7 @@ from .models import Question
 
 
 def index(request):
+
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #template = loader.get_template('polls/index.html')
     context = {
@@ -15,7 +18,14 @@ def index(request):
     return render(request, 'oil/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are at question %s." % question_id)
+#    try:
+#        question = Question.objects(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'oil/detail.html', {'question': question})
+#
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'oil/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are lokking at the results of question %s"
